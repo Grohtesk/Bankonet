12ba86467bc30fdb6b0a07f7cbe11ff98b00cedb
Simon dePonf <simon.depontfarcy@gmail.com>
2015-10-13 11:13:04 +0200

    TP8
    Pareil avec des HashMap à la place des ArrayList

--- src/com/bankonet/Client.java
+++ src/com/bankonet/Client.java
@@ -1,6 +1,7 @@
 package com.bankonet;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,6 +10,7 @@ public class Client {
 	private String prenom;
 	private String identifiant;
 	private List<Compte> comptesList;
+	private HashMap<String, Compte> compteMap;
 	
 	
 	public Client(String nom, String prenom, String identifiant) {
@@ -17,6 +19,7 @@ public class Client {
 		this.prenom = prenom;
 		this.identifiant = identifiant;
 		this.comptesList=new ArrayList<Compte>();
+		this.compteMap=new HashMap<>();
 	}
 
 
@@ -44,27 +47,36 @@ public class Client {
 	}
 	
 	public void creerCompte(Compte compte) {
+		this.compteMap.put(compte.getNumero(), compte);
 		this.comptesList.add(compte);
 	}
 
 	public void supprimerCompte(Compte compte){
+		this.compteMap.remove(compte.getNumero());
 		this.comptesList.remove(compte);
 	}
 	
 	public void supprimerCompte(String numero) throws CompteNonTrouveException{
 		Compte compte=retournerCompte(numero);
 		this.comptesList.remove(compte);
+		this.compteMap.remove(numero);
 	}
 	
 	public Compte retournerCompte(String numero) throws CompteNonTrouveException {
-		Iterator<Compte> iterator=comptesList.iterator();
-		while (iterator.hasNext()) {
-			Compte compte = (Compte) iterator.next();
-			if(compte.getNumero()==numero){
-				return compte;
-			}
+		if(this.compteMap.get(numero)!=null){
+			return (Compte)this.compteMap.get(numero);
+		}else{
+			throw new CompteNonTrouveException("Compte non trouvé pour le n°"+numero);
 		}
-		throw new CompteNonTrouveException("Compte non trouvé pour le n°"+numero);
+		
+//		Iterator<Compte> iterator=comptesList.iterator();
+//		while (iterator.hasNext()) {
+//			Compte compte = (Compte) iterator.next();
+//			if(compte.getNumero()==numero){
+//				return compte;
+//			}
+//		}
+//		throw new CompteNonTrouveException("Compte non trouvé pour le n°"+numero);
 	}
 	
 	
